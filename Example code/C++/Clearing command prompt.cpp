1d8e45dabaf39f983da563c39d718a60dcfedb60
Angel Flores <pure.angel.alexis.flores@gmail.com>
2017-10-02 08:34:34 -0700

    Clearing command promt
    Showing code used to clear command prompt in CLI.

---
+++ Example code/C++/Clearing command prompt.cpp
@@ -0,0 +1,17 @@
+/*
+Angel Flores
+Project Example Code
+2017/10/2 8:16 AM
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+ char hold = ' ';
+ for(int i = 0;i < 10;i++)
+ {
+  system("cls");
+  cout << "Iteration: " << i << '\n';
+ }
+ cin >> hold;
+}
